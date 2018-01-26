51a4aaed053876cf6a3bc1ee0f88c6bc3b39ce60
Lauan Borges Guermandi <lauanguermandi@gmail.com>
2018-01-25 22:39:46 -0200

    start structure of bot

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ define( 'ABSPATH', __DIR__ . "/");
 
 //fake token
 define( 'VERIFY_TOKEN', '0sgdG870SG09GsG7S9GS90PSG990PSVGF' );
-define( 'PAGE_ACCESS_TOKEN', 'EAAQIfFIah8gBADDFGYJlKQDowM2a8SrvwCNSdZAk6n9b25pHDOvjUitv3nAuYbHv7uA5AWQ6MLHszeMRnwgZBDmqtbnfk0MfHqYRQNBbpc6ZBDdZAgcXyneZADp1KZCw3ZA5zIE5TgV6aEj8r8ZAZBCpWUAKU31qGTCW7BkjZBxmvF6WJpRFZCaYQbD' );
+define( 'PAGE_ACCESS_TOKEN', 'asda' );
 
 new Route();
 
